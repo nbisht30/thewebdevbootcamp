bed0cd42f38a5ef2d5baca0c6cfef3bedc601fe5
nbisht30 <nbisht30@gmail.com>
2020-05-24 17:42:40 +0530

    JS - Stalker Exercise

---
+++ JS/Methods/script.js
@@ -0,0 +1,5 @@
+var fn = prompt("What is your first name?");
+var ln = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+console.log("Your full name is " + fn + " " + ln + "\nYour age is "+ age + " years" );
---
+++ JS/StalkerExercise/script.js
@@ -0,0 +1,5 @@
+var userName = prompt("What is your name?");
+
+alert("Hello from the JS file: " + userName);
+
+console.log("Printing name on console: " + userName);
