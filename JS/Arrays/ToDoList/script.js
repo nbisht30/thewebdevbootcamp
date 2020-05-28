73f3e424bde6d566b55854150ce4e540f0e149a8
nbisht30 <nbisht30@gmail.com>
2020-05-28 19:10:16 +0530

    JS - Arrays | Foreach and Deleting items from arrays

--- JS/Arrays/ToDoList/script.js
+++ JS/Arrays/ToDoList/script.js
@@ -5,10 +5,18 @@ function listJS() {
         if (choice === "new") {
             var item = prompt("Enter a new todo:");
             items.push(item);
+            console.log("Item added.");
         } else if (choice === "list") {
-            console.log("List: " + items);
+            console.log("List of items:-")
+            items.forEach(function (todo, i) {
+                console.log((i + 1) + ". " + todo);
+            });
+        } else if (choice === "delete") {
+            var index = prompt("Enter index of todo to delete: ");
+            items.splice(index, 1); // to delete n items starting from index.
+            console.log("Item deleted.");
         }
-    } while (choice === "new" || choice === "list");
+    } while (choice !== "quit");
 }
 
 window.setTimeout(listJS, 500);
