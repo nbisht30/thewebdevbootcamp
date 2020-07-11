8f29c8eb0c2d18682bc0f6269cbfeb964e87b92d
nbisht30 <nbisht30@gmail.com>
2020-07-11 12:53:47 +0530

    Color Game Project | Part 4

--- JS/ColorGameProject/colorgame.js
+++ JS/ColorGameProject/colorgame.js
@@ -4,8 +4,25 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
 
 colorDisplay.textContent = pickedColor;
+
+resetButton.addEventListener("click", function(){
+    //generate all new colors
+    colors = getRandomColorArray(6);
+    //pick a new random color from array
+    pickedColor = pickColor();
+    //change colorDisplauy to match picked color
+    colorDisplay.textContent = pickedColor;
+    //change colors of squares
+    for (let i = 0; i < colors.length; i++) {
+        // Adding initial colors to squares.
+        squares[i].style.backgroundColor = colors[i];
+    }
+    //change background of h1
+    h1.style.backgroundColor = "#232323";
+});
 
 for (let i = 0; i < colors.length; i++) {
     // Adding initial colors to squares.
@@ -17,6 +34,7 @@ for (let i = 0; i < colors.length; i++) {
             messageDisplay.textContent = "Correct!";
             changeColors(pickedColor);
             h1.style.backgroundColor = pickedColor;
+            resetButton.textContent = "Play Again?"
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try Again.";
